32a372bd7223f2fffc3c549f168aab4c8017ce4b
Jean-Pierre Fouche <jeanpierrefouche@gmail.com>
2024-02-21 13:39:51 +0000

    latest

--- python/src/challenges/lambda_fibonacci.py
+++ python/src/challenges/lambda_fibonacci.py
@@ -1,11 +1,18 @@
 from functools import reduce
 
+  
+append_fib = lambda x, _: x + [x[-1] + x[-2]]
 
-    
-# append_fib = lambda x, _: x + [x[-1] + x[-2]]
-# def get_fib(n):
-#     return reduce(append_fib, range(n-2), [0,1])
+# using functools.reduce
+def get_fib_1(n):
+    if n < 0:
+        raise IndexError
+    fib = [0,1]
+    if n < 2:
+        return fib[:n]
+    return reduce(append_fib, range(n-2), fib)
 
+#using simple implementation
 def get_fib(n):
     if n < 0:
         raise IndexError
@@ -17,4 +24,4 @@ def get_fib(n):
     return fib
 
 if __name__ == '__main__':
-    print(get_fib(5))
+    print(get_fib_1(5))
--- python/tests/tests_lambda_reduce_fibonacci.py
+++ python/tests/tests_lambda_reduce_fibonacci.py
@@ -5,39 +5,46 @@ import unittest, sys, os
 current_dir = os.path.dirname(__file__)
 project_dir = os.path.join('..', current_dir)
 sys.path.append(project_dir)
+module = __import__(__name__) # We are importing get_fib 
+# into the local module, so we need a reference to local 
+# module in order to use getattr below
 
 from src.challenges.lambda_fibonacci import get_fib
+from src.challenges.lambda_fibonacci import get_fib_1
 
 class TestGetFib(unittest.TestCase):
     def test_get_fib(self):
-        # Test with n = 0
-        expected = []
-        actual = get_fib(0)
-        self.assertEqual(actual, expected)
+        functions = ['get_fib', 'get_fib_1'] # test both implementations
+        for function in functions:
+            get_fib = getattr(module, function)
+            # Test with n = 0
+            expected = []
+            actual = get_fib(0)
+            self.assertEqual(actual, expected)
 
-        # Test with n = 1
-        expected = [0]
-        actual = get_fib(1)
-        self.assertEqual(actual, expected)
+            # Test with n = 1
+            expected = [0]
+            actual = get_fib(1)
+            self.assertEqual(actual, expected)
 
-        # Test with n = 2
-        expected = [0, 1]
-        actual = get_fib(2)
-        self.assertEqual(actual, expected)
+            # Test with n = 2
+            expected = [0, 1]
+            actual = get_fib(2)
+            self.assertEqual(actual, expected)
 
-        # Test with n = 5
-        expected = [0, 1, 1, 2, 3]
-        actual = get_fib(5)
-        self.assertEqual(actual, expected)
+            # Test with n = 5
+            expected = [0, 1, 1, 2, 3]
+            actual = get_fib(5)
+            self.assertEqual(actual, expected)
 
-        # Test with n = 10
-        expected = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
-        actual = get_fib(10)
-        self.assertEqual(actual, expected)
+            # Test with n = 10
+            expected = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+            actual = get_fib(10)
+            self.assertEqual(actual, expected)
 
-        # Test with negative n
-        with self.assertRaises(IndexError):
-            get_fib(-1)
+            # Test with negative n
+            with self.assertRaises(IndexError):
+                get_fib(-1)
 
 if __name__ == '__main__':
     unittest.main()
